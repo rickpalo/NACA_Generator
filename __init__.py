2c52d0a3165242e6b362a76e27a4069d3d500b78
rickpalo <38861886+rickpalo@users.noreply.github.com>
2018-10-27 13:49:56 -0600

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,15 @@
+bl_info = {
+    "name": "NACA_Generator",
+    "author": "Rick Palo",
+    "version": (0, 0, 1),
+    "blender": (2, 7, 9),
+    "location": "View_3D > Create > NACA_Generator",
+    "description": "Creates Airfoil curve or surface",
+    "warning": "",
+    "wiki_url": ",
+    "tracker_url": "",
+    "category": "Airfoils"}
+    
+    #create the GUI panel
+    class NACAGenPanel(bpy.types.Panel):
+      #TODO
